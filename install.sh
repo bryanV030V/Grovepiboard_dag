90359eac4c39388464aaceabf5703d9bf47eaeb1
Bryan Van 't veld <61687852+bryanV030V@users.noreply.github.com>
2024-01-30 15:35:20 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ sudo apt --fix-broken install
 curl -kl curl -kL dexterindustries.com/update_grovepi | bash
 cd /home/pi/grove/Dexter/GrovePi/Firmware
 bash firmware_update.sh
+
+echo "maakt directory genaamt grove"
+echo "vervolgens zorgt hij er voor dat de software word gedownload en gebuild."
+echo "dan fixt hij de download doe fout is gegaan en start hij hem opnieuw"
+echo "nu lukt het wel aangezien hij niet meer hoeft te builden allen plaatsen"
+echo "en tot slot update hij alle firmware."
+
 # maakt directory genaamt grove
 # vervolgens zorgt hij er voor dat de software word gedownload en gebuild.
 # dan fixt hij de download doe fout is gegaan en start hij hem opnieuw
